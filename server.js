d29a1beddbc91d899f4f6be63eb168f5e299beac
Quan Huynh <quanhuynh364@gmail.com>
2026-05-05 17:15:03 -0400

    implement add new article

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const articleRouter = require("./routes/articles")
 const app = express();
 app.set('view engine', 'ejs');
 
-app.use('articles', articleRouter);
+app.use('/articles', articleRouter);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const articles = [{
+        title: "Test Article",
+        createdAt: new Date(),
+        description: 'Text Description'
+    },
+    {
+        title: "Test Article2",
+        createdAt: new Date(),
+        description: 'Text Description'
+    }]
+    res.render('articles/index', { articles: articles });
 })
 
 app.listen(3000);
